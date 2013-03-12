39936ad9ab2c8ef46c16b4f4e9d3606efba1f4f2
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2013-03-12 19:56:04 +0000

    В SystemStateViewModel добавили недостающих свойств

--- systemstateviewmodel.h
+++ systemstateviewmodel.h
@@ -16,15 +16,27 @@ class SystemStateViewModel : public QDeclarativeItem
     int getspeedRestrictionValue;
     Q_PROPERTY(int SpeedRestriction READ getSpeedRestriction WRITE setSpeedRestriction NOTIFY SpeedRestrictionChanged)
 
+    // Целевая скорость
+    int gettargetSpeedValue;
+    Q_PROPERTY(int TargetSpeed READ getTargetSpeed WRITE setTargetSpeed NOTIFY TargetSpeedChanged)
+
+    // Ускорение
+    double getaccelerationValue;
+    Q_PROPERTY(double Acceleration READ getAcceleration WRITE setAcceleration NOTIFY AccelerationChanged)
+
+    // ТЦ
+    QString getpressureTCValue;
+    Q_PROPERTY(QString PressureTC READ getPressureTC WRITE setPressureTC NOTIFY PressureTCChanged)
+
+    // ТМ
+    QString getpressureTMValue;
+    Q_PROPERTY(QString PressureTM READ getPressureTM WRITE setPressureTM NOTIFY PressureTMChanged)
+
     double getlongitudeValue;
     Q_PROPERTY(double Longitude READ getLongitude WRITE setLongitude NOTIFY LongitudeChanged)
 
     double getlatitudeValue;
     Q_PROPERTY(double Latitude READ getLatitude WRITE setLatitude NOTIFY LatitudeChanged)
-
-    // Необходимость подтверждения бдительности
-    bool getisVigilanceRequiredValue;
-    Q_PROPERTY(bool IsVigilanceRequired READ getIsVigilanceRequired WRITE setIsVigilanceRequired NOTIFY IsVigilanceRequiredChanged)
 
     // Общий уровень предупреждений
     int getsystemWarningLevelValue;
@@ -66,6 +78,22 @@ class SystemStateViewModel : public QDeclarativeItem
     QString getdateValue;
     Q_PROPERTY(QString Date READ getDate WRITE setDate NOTIFY DateChanged)
 
+    // Наличие кассеты регистрации
+    bool getisRegistrationTapeActiveValue;
+    Q_PROPERTY(bool IsRegistrationTapeActive READ getIsRegistrationTapeActive WRITE setIsRegistrationTapeActive NOTIFY IsRegistrationTapeActiveChanged)
+
+    // Режим движения (М = маневровый, П = поездной)
+    QString getdriveModeValue;
+    Q_PROPERTY(QString DriveMode READ getDriveMode WRITE setDriveMode NOTIFY DriveModeChanged)
+
+    // Необходимость подтверждения бдительности
+    bool getisVigilanceRequiredValue;
+    Q_PROPERTY(bool IsVigilanceRequired READ getIsVigilanceRequired WRITE setIsVigilanceRequired NOTIFY IsVigilanceRequiredChanged)
+
+    // Режим движения (-1 = назад, 0 = стоим, +1 = вперёд)
+    int getdirectionValue;
+    Q_PROPERTY(int Direction READ getDirection WRITE setDirection NOTIFY DirectionChanged)
+
     // Неведомо чудо
     bool getpropertyViewValue;
     Q_PROPERTY(bool PropertyView READ getPropertyView WRITE setPropertyView NOTIFY PropertyViewChanged)
@@ -78,9 +106,12 @@ public:
     // public properties getters start
     const double getSpeed() const;
     const int getSpeedRestriction() const;
+    const int getTargetSpeed() const;
+    const double getAcceleration() const;
+    const QString getPressureTC() const;
+    const QString getPressureTM() const;
     const double getLongitude() const;
     const double getLatitude() const;
-    const bool getIsVigilanceRequired() const;
     const int getSystemWarningLevel() const;
     const int getFullSetWarningLevel() const;
     const bool getIsPressureOk() const;
@@ -91,6 +122,10 @@ public:
     const int getAlsnFreq() const;
     const QString getTime() const;
     const QString getDate() const;
+    const bool getIsRegistrationTapeActive() const;
+    const QString getDriveMode() const;
+    const bool getIsVigilanceRequired() const;
+    const int getDirection() const;
     const bool getPropertyView() const;
     // public properties getters end
 
@@ -98,9 +133,12 @@ signals:
     // properties signals start
     void SpeedChanged();
     void SpeedRestrictionChanged();
+    void TargetSpeedChanged();
+    void AccelerationChanged();
+    void PressureTCChanged();
+    void PressureTMChanged();
     void LongitudeChanged();
     void LatitudeChanged();
-    void IsVigilanceRequiredChanged();
     void SystemWarningLevelChanged();
     void FullSetWarningLevelChanged();
     void IsPressureOkChanged();
@@ -111,6 +149,10 @@ signals:
     void AlsnFreqChanged();
     void TimeChanged();
     void DateChanged();
+    void IsRegistrationTapeActiveChanged();
+    void DriveModeChanged();
+    void IsVigilanceRequiredChanged();
+    void DirectionChanged();
     void PropertyViewChanged();
     // properties signals end
 
@@ -118,9 +160,12 @@ public slots:
     // public properties setters start
     void setSpeed(const double);
     void setSpeedRestriction(const int);
+    void setTargetSpeed(const int);
+    void setAcceleration(const double);
+    void setPressureTC(const QString);
+    void setPressureTM(const QString);
     void setLongitude(const double);
     void setLatitude(const double);
-    void setIsVigilanceRequired(const bool);
     void setSystemWarningLevel(const int);
     void setFullSetWarningLevel(const int);
     void setIsPressureOk(const bool);
@@ -131,6 +176,10 @@ public slots:
     void setAlsnFreq(const int);
     void setTime(const QString);
     void setDate(const QString);
+    void setIsRegistrationTapeActive(const bool);
+    void setDriveMode(const QString);
+    void setIsVigilanceRequired(const bool);
+    void setDirection(const int);
     void setPropertyView(const bool);
     // public properties setters end
     

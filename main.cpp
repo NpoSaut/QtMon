9dea853c5dd3abfd50caa202286d23863375e530
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2014-12-05 14:37:21 +0500

    Исправлена ошибка в выключении светофора по ключу ЭПК - опечатка в Connect

--- main.cpp
+++ main.cpp
@@ -329,7 +329,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     //Светофоры:
     // огонь
     trafficlightAdaptor = new TrafficLightOnOffAdaptor();
-    QObject::connect (&blokMessages->mcoState, SIGNAL(epvReadyChanged(bool)), (TrafficLightOnOffAdaptor*)trafficlightAdaptor, SLOT(setOnState()));
+    QObject::connect (&blokMessages->mcoState, SIGNAL(epvReadyChanged(bool)), (TrafficLightOnOffAdaptor*)trafficlightAdaptor, SLOT(setOnState(bool)));
     QObject::connect (&blokMessages->mcoState, SIGNAL(trafficlightChanged(Trafficlight)), trafficlightAdaptor, SLOT(proccessNewTrafficlight(Trafficlight)));
     QObject::connect(trafficlightAdaptor, SIGNAL(trafficlightCodeChanged(int)), systemState, SLOT(setLight(int)));
     ledTrafficlight = new LedTrafficlight (gpioProducer);

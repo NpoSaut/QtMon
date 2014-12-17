a99ba66c0f2e6795a6521e65cee390c24909f3f1
Evgeniy Plyusnin <eninbox@gmail.com>
2014-12-17 18:46:30 +0500

    Делегировал смену активностий и уничтожение контекста истории, а менеджеру историй -- смену и уничтожение историй. Все контексты пришлось унаследовать от одного класса Context, чтобы проще было хранить

--- interaction/story.cpp
+++ interaction/story.cpp
@@ -2,8 +2,31 @@
 
 namespace Interaction {
 
-Story::Story()
-    : QList ()
+Story::Story(Context *storyContext, QList<Activities::Activity> activities)
+    : context(storyContext), currentActivityIndex(-1), activities(activities)
 { }
+
+void Story::SwitchNext()
+{
+    if (currentActivityIndex > 0)
+        activities[currentActivityIndex].dispose();
+    if (currentActivityIndex < activities.count())
+    {
+        currentActivityIndex++;
+        activities[currentActivityIndex].run();
+    }
+}
+
+void Story::Dispose()
+{
+    for (int i = currentActivityIndex; i < activities.count(); i++)
+    {
+        // вроде как, мы принмаем этот список как копию,
+        // поэтому можно не удалять из него ничего, да?
+        activities[i].dispose();
+    }
+    if (context != nullptr)
+        delete context;
+}
 
 }

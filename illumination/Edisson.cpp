3a3a92d7b7d03925fc8248165f6265874cfb5c60
Evgeniy Plyusnin <eninbox@gmail.com>
2015-05-28 10:11:34 +0500

    Поддержка настроек и текущего значения в Edisson

--- illumination/Edisson.cpp
+++ illumination/Edisson.cpp
@@ -1,12 +1,23 @@
 #include "Edisson.h"
 
-Edisson::Edisson(IAnalogDevice *outputDevice, IIntensityConverter *intensityConverter) :
-    _outputDevice(outputDevice), _intensityConverter(intensityConverter)
+Edisson::Edisson(IAnalogDevice *outputDevice, IIntensityConverter *intensityConverter, IIlluminationSettings *settings) :
+    _currentIllumination(-1), _outputDevice(outputDevice), _intensityConverter(intensityConverter), _settings(settings)
 {
+    double storedIllumination = _settings->getIlluminationValue();
+    setIllumination(storedIllumination);
+}
+
+double Edisson::Illumination()
+{
+    return _currentIllumination;
 }
 
 void Edisson::setIllumination(double value)
 {
+    if (value == _currentIllumination) return;
+    _currentIllumination = value;
+    _settings->storeIlluminationValue(value);
+
     double rangeValue = _intensityConverter->toRange(value);
     _outputDevice->setValue(rangeValue);
 }
--- illumination/Edisson.h
+++ illumination/Edisson.h
@@ -3,15 +3,19 @@
 
 #include "interfaces/IAnalogDevice.h"
 #include "interfaces/IIntensityConverter.h"
+#include "interfaces/IIlluminationSettings.h"
 
 class Edisson
 {
+    double _currentIllumination;
     IAnalogDevice *_outputDevice;
     IIntensityConverter *_intensityConverter;
+    IIlluminationSettings *_settings;
 
 public:
-    Edisson(IAnalogDevice *outputDevice, IIntensityConverter *intensityConverter);
+    Edisson(IAnalogDevice *outputDevice, IIntensityConverter *intensityConverter, IIlluminationSettings *settings);
 
+    double Illumination();
     void setIllumination(double value);
 
 private:

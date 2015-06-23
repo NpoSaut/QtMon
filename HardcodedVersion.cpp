f58198891dc9b62ab392fe16e59ffac30b04069a
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-06-23 09:09:55 +0500

    В вывод захаркоженной версии добавлена контрльная сумма - считается по главному исполняемому файлу

--- HardcodedVersion.cpp
+++ HardcodedVersion.cpp
@@ -1,10 +1,20 @@
 #include "HardcodedVersion.h"
+#include <QFile>
+#include <QCoreApplication>
+#include <QCryptographicHash>
 
 HardcodedVersion::HardcodedVersion(int version, int subversion, Can *can, QObject *parent) :
     QObject(parent), can (can), aux (AuxResource::Descriptor::BIL_A, this)
 {
     aux.setVersion(version);
     aux.setSubversion(subversion);
+
+    QFile mainFile(QCoreApplication::applicationFilePath());
+    if (mainFile.open(QIODevice::ReadOnly))
+    {
+        QByteArray cs = QCryptographicHash::hash(mainFile.readAll(), QCryptographicHash::Sha1);
+        aux.setChecksum(int(cs[1])*256 + cs[0]);
+    }
 
     can->transmitMessage(aux.encode());
 }
